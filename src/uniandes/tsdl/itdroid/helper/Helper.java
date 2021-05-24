42c3bb66c088a3b23d48863ea0881cd1f5aa870c
Andres Donoso <af.donoso@uniandes.edu.co>
2021-05-24 00:48:28 -0500

    Started impementation of support for RTL languages

--- src/uniandes/tsdl/itdroid/helper/Helper.java
+++ src/uniandes/tsdl/itdroid/helper/Helper.java
@@ -23,58 +23,71 @@ import org.xml.sax.SAXException;
 
 public class Helper {
 
-	public static Helper instance = null;
-	public static String currDirectory = "";
-	public static List<String> actNames = new ArrayList<String>();
-	public static String mainActivity = "";
-	public static String packageName = "";
-	public final static String MANIFEST = "AndroidManifest.xml";
-	public final static String MAIN_ACTION = "android.intent.action.MAIN";
-	public static final int MIN_VERSION = 2;
-	public static final int MAX_VERSION = 27;
-	public static final String MIN_SDK_VERSION = "android:minSdkVersion";
-	public static final String TARGET_SDK_VERSION = "android:targetSdkVersion";
-	public static final String MAX_SDK_VERSION = "android:maxSdkVersion";
-	public static final String STRINGS = "strings.xml";
-	public static final String COLORS = "colors.xml";
+    public static Helper instance = null;
+    public static String currDirectory = "";
+    public static List<String> actNames = new ArrayList<String>();
+    public static String mainActivity = "";
+    public static String packageName = "";
+    public final static String MANIFEST = "AndroidManifest.xml";
+    public final static String MAIN_ACTION = "android.intent.action.MAIN";
+    public static final int MIN_VERSION = 2;
+    public static final int MAX_VERSION = 27;
+    public static final String MIN_SDK_VERSION = "android:minSdkVersion";
+    public static final String TARGET_SDK_VERSION = "android:targetSdkVersion";
+    public static final String MAX_SDK_VERSION = "android:maxSdkVersion";
+    public static final String STRINGS = "strings.xml";
+    public static final String COLORS = "colors.xml";
 
-	public static Helper getInstance() {
-		if (instance == null) {
-			instance = new Helper();
-		}
-		return instance;
-	}
+    // LIST OF CODES FOR RTL LANGUAGES
+    // ar = Arabic
+    // arc = Aramaic
+    // ckb = Kurdish (Sorani)
+    // dv = Divehi
+    // fa = Persian
+    // ha = Hausa
+    // he = Hebrew
+    // khw = Khowar
+    // ks = Kashimiri
+    // ps = Pashto
+    // ur = Urdu
+    // uz_AF = Uzbeki Afghanistan
+    // yi = Yidish
+    public static final String[] RTL_LANGUAGES = { "ar", "arc", "ckb", "dv", "fa", "ha", "he", "khw", "ks", "ps", "ur",
+            "uz_AF", "yi" };
 
-	public static String getPackageName() {
-		return packageName;
-	}
+    public static Helper getInstance() {
+        if (instance == null) {
+            instance = new Helper();
+        }
+        return instance;
+    }
 
+    public static String getPackageName() {
+        return packageName;
+    }
 
+    public static void setPackageName(String packageName) {
+        Helper.packageName = packageName;
+    }
 
-	public static void setPackageName(String packageName) {
-		Helper.packageName = packageName;
-	}
+    public String getCurrentDirectory() throws UnsupportedEncodingException {
 
+        String dir = System.getProperty("user.dir");
+        return dir;
 
+    }
 
-	public String getCurrentDirectory() throws UnsupportedEncodingException {
+    public static boolean isWindows() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.indexOf("win") >= 0) {
+            return true;
+        }
+        return false;
+    }
 
-		String dir = System.getProperty("user.dir");
-		return dir;
-
-	}
-
-	public static boolean isWindows() {
-		String os = System.getProperty("os.name").toLowerCase();
-		if (os.indexOf("win") >= 0) {
-			return true;
-		}
-		return false;
-	}
-
-	public static int levenshteinDistance(String left, String right) {
-		left = left.toLowerCase();
-		right = right.toLowerCase();
+    public static int levenshteinDistance(String left, String right) {
+        left = left.toLowerCase();
+        right = right.toLowerCase();
         // i == 0
         int n = left.length(); // length of left
         int m = right.length(); // length of right
@@ -124,5 +137,21 @@ public class Helper {
         }
 
         return p[n];
+    }
+
+    /**
+     * Indicates whether a language is RTL.
+     * 
+     * @param code ISO code for the language
+     * @return True if the language is RTL, false otherwise.
+     */
+    public static boolean languageIsRTL(String code) {
+        for (String languageCode : RTL_LANGUAGES) {
+            if (languageCode.equals(code)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- src/uniandes/tsdl/itdroid/model/GraphEdgeType.java
+++ src/uniandes/tsdl/itdroid/model/GraphEdgeType.java
@@ -4,20 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public enum GraphEdgeType {
-	ABOVE,
-	BELOW,
-	LEFT,
-	RIGHT,
-	TOP_ALIGNED,
-	BOTTOM_ALIGNED,
-	RIGHT_ALIGNED,
-	LEFT_ALIGNED,
-	CONTAINS,
-	CONTAINED,
-	INTERSECTS,
-	DEFAULT;
+	ABOVE, BELOW, LEFT, RIGHT, TOP_ALIGNED, BOTTOM_ALIGNED, RIGHT_ALIGNED, LEFT_ALIGNED, CONTAINS, CONTAINED,
+	INTERSECTS, DEFAULT;
 
-	
 	public static Set<GraphEdgeType> getAligmentTypes() {
 		Set<GraphEdgeType> set = new HashSet<GraphEdgeType>();
 		set.add(TOP_ALIGNED);
@@ -26,7 +15,7 @@ public enum GraphEdgeType {
 		set.add(RIGHT_ALIGNED);
 		return set;
 	}
-	
+
 	public static Set<GraphEdgeType> getDirectionTypes() {
 		Set<GraphEdgeType> set = new HashSet<GraphEdgeType>();
 		set.add(ABOVE);
@@ -35,6 +24,13 @@ public enum GraphEdgeType {
 		set.add(RIGHT);
 		return set;
 	}
-	
-}
 
+	public static Set<GraphEdgeType> getRtlChangeExpectedTypes() {
+		Set<GraphEdgeType> set = new HashSet<>();
+		set.add(LEFT);
+		set.add(RIGHT);
+		set.add(LEFT_ALIGNED);
+		set.add(RIGHT_ALIGNED);
+		return set;
+	}
+}
